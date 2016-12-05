af4bc3292e23a69c45a8c31bcde3bb7bb2eda5c1
sermo <renqing50@gmail.com>
2016-12-05 12:35:08 +0800

    first

---
+++ first_program/inputOrOutput.py
@@ -0,0 +1,11 @@
+print('hello world') # hello world
+print('one','two') # one two
+print('100 + 200 =', 100 + 200)
+
+# 输入
+name=input()
+print(name)
+
+name=input("enter your name: ")
+print('hello',name)
+
